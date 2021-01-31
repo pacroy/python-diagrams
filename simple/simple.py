4bac2989ff708cba874b60ab4eba0801401d88d9
Par <pacroy@gmail.com>
2021-01-31 12:13:28 +0700

    Remove files from root

--- simple/simple.py
+++
@@ -1,5 +0,0 @@
-from diagrams import Diagram
-
-with Diagram("Simple Website Diagram") as diag:
-    pass
-diag # This will illustrate the diagram if you are using a Google Colab or Jypiter notebook.
