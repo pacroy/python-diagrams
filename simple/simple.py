a1e530e3980721506e42ddfb3e79f3028910eac5
Par <pacroy@gmail.com>
2021-01-31 12:14:24 +0700

    Revert "Remove files from root"
    This reverts commit 4bac2989ff708cba874b60ab4eba0801401d88d9.

---
+++ simple/simple.py
@@ -0,0 +1,5 @@
+from diagrams import Diagram
+
+with Diagram("Simple Website Diagram") as diag:
+    pass
+diag # This will illustrate the diagram if you are using a Google Colab or Jypiter notebook.
